4b079f71ddb0359c5afe915f787dcc5e55bf9eae
dclarkasu <dclark205@gmail.com>
2017-08-05 18:10:26 -0600

    Editting and Comments

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,35 +1,16 @@
-
-//User Story #1
-//
-//The user is prompted asking for the price of the item.
-//
-//User Story #2
-//
-//The user is then prompted asking how much money was tendered by the customer.
-//
-//User Story #3
-//
-//Display an appropriate message if the customer provided too little money or the exact amount.
-//
-//User Story #4
-//
-//If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
-//how many of each piece of currency ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed to make the change for the customer.
-//Hints: % operator and using pennies as a base?
-
 import java.util.Scanner;
 
 public class MakeChange {
-	static int[] cashAmt = { 2000, 1000, 500, 100, 25, 10, 5, 1 };			//array containing doubles for doing actual calculations
-	static String[] bills = { "$20's", "$10's", "$5's", "$1's", ".25c", ".10c", ".05c", ".01c" };  //array contains Strings so that it outputs labels instead of just the digit for how many are needed
-	static String[] bill = { "$20", "$10", "$5", "$1", ".25c", ".10c", ".05c", ".01c" };  //bill is singular b/c it is for when there is only 1 required $ unit
+	static int[] cashAmt = { 2000, 1000, 500, 100, 25, 10, 5, 1 };//array containing doubles for doing actual calculations
+	static String[] bills = { "$20's", "$10's", "$5's", "$1's", ".25c", ".10c", ".05c", ".01c" };//array contains Strings so that it outputs labels instead of just the digit for how many are needed
+	static String[] bill = { "$20", "$10", "$5", "$1", ".25c", ".10c", ".05c", ".01c" }; //bill is singular b/c it is for when there is only 1 required $ unit
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);		//declared in main so must be pased into methods. (Doesn't have to have same name)
+		Scanner scanner = new Scanner(System.in);		//declared in main so must be passed into methods. (Doesn't have to have same name)
 		char answer;									//made a char in order to better check for their answer
 
-		do {											//enter loop
-			runTransaction(scanner);					//calls method and passes in scanner. Jump to line 36
+		do {											
+			runTransaction(scanner);					//Jump to line 36
 			System.out.println();
 			System.out.println("Would you like to make another purchase (Y/N)?");
 			answer = scanner.next().toLowerCase().charAt(0);
@@ -38,7 +19,7 @@ public class MakeChange {
 
 	public static void runTransaction(Scanner kb) {
 		double price, tender;
-		int changeToGive;												//gets casted to an int later
+		int changeToGive;											
 
 		// Prompt: customer asks how much item is
 		System.out.println("\n" + "Excuse me, how much is this item?");
@@ -46,7 +27,7 @@ public class MakeChange {
 		price = kb.nextDouble();
 		System.out.println();
 
-		// Prompt how much money was tendered by customer
+		// Prompt: how much money was tendered by customer
 		System.out.print("Thanks, here is my money: ");
 		tender = kb.nextDouble();
 
@@ -57,28 +38,25 @@ public class MakeChange {
 			System.out.println("Thank you, you've given me the exact amount");
 		} else {
 			System.out.println("Thank you, let me get your change");
-			changeToGive = (int) ((100 * tender) - (100 * price));
+			changeToGive = (int) ((100 * tender) - (100 * price)); 
+			//if we don't multiply by 100 we won't get the right amount of cents when the subtraction occurs
 			createChange(changeToGive);
 		}
 	}
 
-	public static void createChange(double changeToGive) {
-		for (int i = 0; i < cashAmt.length; i++) {
-			int amounts = (int) (changeToGive / cashAmt[i]);
-			changeToGive = changeToGive % cashAmt[i];
-			if (amounts > 1) {
-				System.out.println(amounts + " " + bills[i]);
-			} else if (amounts == 1) {
+	public static void createChange(int changeToGive) {
+		for (int i = 0; i < cashAmt.length; i++) {            //iterates throughout array
+			int amounts = (int) (changeToGive / cashAmt[i]);  //division yields how much of each value($ unit) will go into the tendered amt. Then, casted to an int
+			changeToGive = changeToGive % cashAmt[i];			//
+			if (amounts > 1) {								//amounts is our counter, keeping track of how much each value
+				System.out.println(amounts + " " + bills[i]);//> 1 so plural "'s"
+			} 
+			else if (amounts == 1) {
 				System.out.println(amounts + " " + bill[i]);
-
 			}
 		}
-
 	}
-
 }
-
-// Make methods, loop for if they want to purchase something else,
 
 // Test conditions for grading: Amount: .67, Tendered: .50, Result: Error
 // message
